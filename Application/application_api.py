aadcb45e5b950a4a7d14306ebba0b699a807cc02
EHolBRole <ekholo.banderole@mail.ru>
2024-07-22 16:16:54 +0300

    Added response convertion from Domain DTO to Application DTO

--- Application/application_api.py
+++ Application/application_api.py
@@ -1,17 +1,18 @@
-import Application.application_dto as ap
+import Application.application_dto as a_dto
 import Domain.domain_dto as d_dto
 import Domain.domain_api as d_api
 import Exceptions.exceptions as ex
 
 
-def PreProcessRequest(request: ap.ApplicationDTORequest):
+def PreProcessRequest(request: a_dto.ApplicationDTORequest):
     is_executed = False
     for c in d_api.available_commands:
         if request.request_data[0] == c.name:
             request.request_data.pop(0)
             d_request = d_dto.DomainDTORequest(c, request.request_data)
             is_executed = True
-            return d_api.ProcessRequest(d_request)
+            domain_response = d_api.ProcessRequest(d_request)
+            return a_dto.ApplicationDTOResponse(domain_response.response_data)
     if not is_executed:
         raise ex.IncorrectCommandException("Invalid command!")
 
